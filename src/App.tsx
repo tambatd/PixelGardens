00048ae73ef3b4f08e3835fda29dfb2a4f97cc11
Devendra <tambatd@outlook.com>
2022-02-14 14:33:15 -0500

    init2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home/home";
-import Garden from "./pages/garden/garden";
-import FlowerList from "./pages/flowerList/flowerList";
+
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Home></Home>
   );
 };
 
